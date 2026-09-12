93a16ba75dc254b326c03ed5584ec542f4fe0d07
AGIL XD <agilnurus2008@gmail.com>
2026-09-12 18:51:48 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,23 @@
 
+import type { Metadata } from "next"
+import { SessionProvider } from "next-auth/react"
+import "./globals.css"
+
+export const metadata: Metadata = {
+  title: "Deployer App",
+  description: "Deploy repo GitHub lu ke Vercel cuma dengan klik",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="id">
+      <body>
+        <SessionProvider>{children}</SessionProvider>
+      </body>
+    </html>
+  )
+}
